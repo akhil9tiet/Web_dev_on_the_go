b266ab4dd384662959a2cd2401ca567c6fcb74c1
Akhil Gupta <akhil9tiet@gmail.com>
2016-07-06 13:23:41 +0530

    final instagram download updated

--- 12. Instagram API _IMAGE_DOWNLOADER_/akhil1.php
+++ 12. Instagram API _IMAGE_DOWNLOADER_/akhil1.php
@@ -4,8 +4,8 @@ ini_set('default_socket_timeout',300);
 session_start();
 /*.............Instagram API Keys.............*/
 
-define("clientID",'d****************************27');
-define("clientSecret",'b*******************************3');
+define("clientID",'d86*************************7'); //use your own clientID
+define("clientSecret",'b9******************************d3');// use your own clientSecret
 define("redirectURI",'http://localhost/php_dev_akhil/8.instagramAPI/akhil1.php');
 //Replace this URI with valid website link here and make a new one for the application in validation page on indtagram developer page
 define("imageDirectory",'instaPics/');//create this folder in the same file where we have index.php
@@ -31,7 +31,8 @@ function printImages($userID,$access_token){
     $results=json_decode($instagramInfo,true);
     
     //parse through images
-    echo '<p>The following pictures will be saved on to your local InstaPics folder</p><br/>';
+    echo '<h1>The following pictures will be saved on to your local InstaPics folder</h1><br/>';
+
     foreach($results['data'] as $items){
         $image_url= $items['images']['low_resolution']['url'];
         echo '<img src="'.$image_url.'"/><br/>';
@@ -41,11 +42,19 @@ function printImages($userID,$access_token){
 
 //save the pictures
 function savePictures($image_url){
+    $arrContextOptions=array(
+    "ssl"=>array(
+        "verify_peer"=>false,
+        "verify_peer_name"=>false,
+    ),
+);  
     $filename= basename($image_url);
     list($true_url) = explode('?', $filename);
-    $destination= imageDirectory.$true_url;   
-	
-	file_put_contents($destination,file_get_contents($image_url));
+    
+    list($search_url)= explode('?',$image_url);
+    
+    $destination= imageDirectory.$true_url;    
+    file_put_contents($destination,file_get_contents($search_url));
 }
 
 if($_GET['code']){
@@ -74,17 +83,30 @@ if($_GET['code']){
     $username= $results['user']['username'];    //username
     $access_token=$results['access_token'];    //access_token
     $userID= $results['user']['id'];           //userid
-    //echo $username." ".$access_token."========> ".$userID;
-    //$userID=getUserID($username,$access_token);
    printImages($userID,$access_token);
 }else{
     //logged out
 ?>
     <!DOCTYPE html>
     <html>
-        <head></head>
+        <head>
+            <title>InstaImageDownloader</title>
+            <meta charset="utf-8"/>
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+            <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,700' rel='stylesheet' type='text/css'>
+            <link rel="stylesheet" type="text/css" href="akhil1.css">
+        </head>
         <body>
-            <a href="https://api.instagram.com/oauth/authorize/?client_id=<?php echo clientID;?>&redirect_uri=<?php echo redirectURI;?>&response_type=code">Login Now</a>
+            <section class="jumbotron">
+                <div class="container">
+                    <div class="row text-center">
+                        <h2>Instagram Image Dowloader</h2>
+                        <h3>Download Images to your local computer.</h3>
+                        <a class="btn btn-primary" href="https://api.instagram.com/oauth/authorize/?client_id=<?php echo clientID;?>&redirect_uri=<?php echo redirectURI;?>&response_type=code" role="button">Login Now</a>
+                    </div>
+                </div>
+            </section>
+            <!--<a href="https://api.instagram.com/oauth/authorize/?client_id=<?php //echo clientID;?>&redirect_uri=<?php //echo redirectURI;?>&response_type=code">Login Now</a>-->
             <hr>
         </body>
     </html>
